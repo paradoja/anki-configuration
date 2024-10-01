e9e0ab18ff418dba1b4db6851aeea964be9cbffd
Abby Henríquez Tejera <paradoja@gmail.com>
2024-10-01 19:57:54 +0200

    Updates

---
+++ tech.js
@@ -0,0 +1,22 @@
+function update () {
+  const tagsToRemove = ['aws', 'tech', 'saa_c03'];
+  const tagTransformations =
+        { 'fundamentals': 'fundamentals' };
+  const transformTag = tag => tagTransformations[tag] || tag.toUpperCase();
+  const addTopic = tag => `<span class="topic">${tag}</span>`;
+
+  tags = globalThis.tags.filter(t => !tagsToRemove.includes(t));
+  const individualTags = tags.map(transformTag).map(addTopic);
+  tagElements = Array.from(document.getElementsByClassName('tags'));
+
+  tagElements.forEach(function (tagElement) {
+    tagElement.innerHTML = `<span class="card-type">${individualTags.join('')}</span>`;
+    tagElement.classList.add('category');
+  });
+
+  const optionals = Array.from(document.getElementsByClassName("optional"));
+  optionals.forEach(e => e.remove());
+}
+globalThis.updater = update;
+
+update();
